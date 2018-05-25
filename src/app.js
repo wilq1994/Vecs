ccd64e8e312882e728fda2f6f12f95d4ab56f707
Bartosz Wilk <bartek94@gmail.com>
2018-05-25 16:04:08 +0200

    Connect room route with redux

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { createStore } from 'redux'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
 import store from './store/store';
 
@@ -11,19 +11,26 @@ import NewRoom from './views/NewRoom';
 import JoinRoom from './views/JoinRoom';
 import Room from './views/Room';
 
+
+const RoomRoute = props => {
+  if(props.isAuthenticated) {
+    return <Room {...props} />
+  } else {
+    return <JoinRoom {...props} />
+  }
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.user.isAuthenticated
+})
+
+
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route exact path="/" component={ NewRoom }/>
-        <Route
-          path="/:room"
-          render={
-            props => store.getState().user.isAuthenticated ?
-                    <Room {...props} /> :
-                    <JoinRoom {...props} />
-          }
-        />
+        <Route exact path="/" component={NewRoom} />
+        <Route path="/:room" render={ withRouter(connect(mapStateToProps)(RoomRoute)) } />
       </Switch>
     </Router>
   </Provider>,
